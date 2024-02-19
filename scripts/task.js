c57602e48c6647e3c05d9155bbb248b518897144
Sahim98 <sahimsalem321@gmail.com>
2024-02-19 20:37:11 +0600

    grand total updated

--- scripts/task.js
+++ scripts/task.js
@@ -1,52 +1,82 @@
-document.addEventListener('click', function(e){
+//let totalBooked = 0;
+document.addEventListener('click', function(e)
+{
 
     const list = ['a1', 'a2', 'a3', 'a4','b1', 'b2', 'b3', 'b4'];
-    let ID = e.target.id;
+    let ID = e.target.id, len = 0;
+
+
+
     if(list.includes(ID))
     {
-        
-    if(setBackgroundColorById(ID))
-    {
-        ID = ID.toUpperCase();  
-        seatFull();
-        let el = document.createElement('div');
-        el.classList.add('flex');
-        el.classList.add('justify-between');
-        el.setAttribute('id',ID);
-        let el1 = document.createElement('p'),
-        el2 = document.createElement('p'),
-        el3 = document.createElement('p');
-        el1.innerText = ID;
-        el2.innerText = 'economy' ;
-        el3.innerText = '550';
-        el.appendChild(el1);
-        el.appendChild(el2);
-        el.appendChild(el3);
-        let parent = document.getElementById('ticket');
-        parent.appendChild(el); 
-        console.log(parent)
-        let val = getTextElementValueById('total-amt');
-        setTextElementValueById('total-amt', val+550);
+        if(setBackgroundColorById(ID))
+        {
+            //totalBooked++;
+            ID = ID.toUpperCase();  
+            seatFull();
+            let el = document.createElement('div');
+            el.classList.add('flex');
+            el.classList.add('justify-between');
+            el.setAttribute('id',ID);
+            let el1 = document.createElement('p'),
+            el2 = document.createElement('p'),
+            el3 = document.createElement('p');
+            el1.innerText = ID;
+            el2.innerText = 'economy' ;
+            el3.innerText = '550';
+            el.appendChild(el1);
+            el.appendChild(el2);
+            el.appendChild(el3);
+            let parent = document.getElementById('ticket');
+            parent.appendChild(el); 
+            let val = getTextElementValueById('total-amt');
+            setTextElementValueById('total-amt', val+550);
+            val = getTextElementValueById('grand-total');
+            setTextElementValueById('grand-total', val+550);
+
+        }
+        else
+        {
+            const box = document.getElementById(ID.toUpperCase());
+            box.remove();
+            seatEmpty();
+            let val = getTextElementValueById('total-amt');
+            setTextElementValueById('total-amt', val-550);
+            val = getTextElementValueById('grand-total');
+            setTextElementValueById('grand-total', val-550);
+        }
     }
-    else
-     {
-        ID = ID.toUpperCase();
-        const box = document.getElementById(ID);
-        console.log(box)
-        box.remove();
-        seatEmpty();
-        let val = getTextElementValueById('total-amt');
-        setTextElementValueById('total-amt', val-550);
+
+})
+
+function checkCoupon(){
+    let coupon = document.getElementById('coupon').value;
+    console.log(coupon)
+    if(coupon == "NEW15" || coupon == "Couple 20")
+    {
+        let e = document.getElementById('coupon-sec');
+        let val = getTextElementValueById('grand-total');
+        e.classList.add('hidden');
+
+        if(coupon == "NEW15")
+        {
+           
+            setTextElementValueById('grand-total', val*.85);
+        }
+        else
+        {
+            setTextElementValueById('grand-total', val*.80);
+        }
     }
 }
 
-})
+
 
 function seatEmpty()
 {
     let seatLeft = 'seats-left';
     let j = getTextElementValueById(seatLeft);
-    console.log('seats left ',j);
+    //console.log('seats left ',j);
     setTextElementValueById(seatLeft,j+1);
 }
 
@@ -54,7 +84,8 @@ function seatFull()
 {
     let seatLeft = 'seats-left';
     let j = getTextElementValueById(seatLeft);
-    console.log('seats left ',j);
+   // console.log('seats left ',j);
     setTextElementValueById(seatLeft,j-1);
 }
    
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -62,10 +62,6 @@ function setTextElementValueById(elementId, value){
     element.innerText = value;
 }
 
-// function getElementTextById(elementId){
-//     const element = document.getElementById(elementId);
-//     const text = element.innerText;
-//     return text;
-// }
+
 
 
